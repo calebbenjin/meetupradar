4dc4da4e12707d61d3f34feae2dd8429d29f582a
calebbenjin <calebben94@gmail.com>
2023-02-08 23:22:40 +0100

    feat: dev config

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Inter } from '@next/font/google';
+
+// Note: The subsets need to use single quotes because the font loader values must be explicitly written literal.
+// eslint-disable-next-line @typescript-eslint/quotes
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <h1>TechMeetups</h1>
+    </>
+  );
+}
