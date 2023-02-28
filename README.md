47325af5b3e59a27b5d5c2b88d46c424488215a9
calebbenjin <calebben94@gmail.com>
2023-02-28 01:25:36 +0100

    feat: homepage added

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-## WELCOM TO EASEPLAN.IO 📃🚀🎯
+## WELCOM TO MEETUPRADAR 📃🚀🎯
 
+MeetupRadar is an open-source web app that allows users to see the location of tech meetups in their area. Using a geocoding API to convert addresses to coordinates. Users can also search for meetups based on keywords, location, or date range using the search bar.
 
 ## Features
 
@@ -25,7 +26,6 @@ yarn dev
 ```
 
 Open `http://localhost:3000` with your browser to see the result.
-
 
 ### Requirements
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,17 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <>
-      <h1>TechMeetups</h1>
-    </>
+    <div className="h-screen w-full flex items-center justify-center">
+      <div className="container mx-auto text-center px-20 w-6/12">
+        <h3 className="text-3xl font-bold mb-4">Meet🚀</h3>
+        <h1 className="text-6xl font-bold">MeetupRadar</h1>
+        <p className="text-lg mt-6">
+          MeetupRadar is an open-source web app that allows users to see the
+          location of tech meetups in their area. Using a geocoding API to
+          convert addresses to coordinates. Users can also search for meetups
+          based on keywords, location, or date range using the search bar.
+        </p>
+      </div>
+    </div>
   );
 }
